3a91ba938e05c1aeafc8ec2b86d4a9db76cf7bf8
Dasun <techsaralk.pro@gmail.com>
2024-05-26 10:35:50 +0530

    create task finished

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 # imports
+from token import OP
 import pydantic
 from fastapi import FastAPI
+from uuid import UUID, uuid4
+from typing import List, Optional
+
+# list (acting like a database)
+tasks: list = []
 
 # initialize the app
 app: FastAPI = FastAPI()
 
+class Task(pydantic.BaseModel):
+    id: Optional[UUID] = None
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
+
 # routers
 @app.get("/")
-async def read() -> dict[str, str]:
-    return {"message": "Hello World from FastAPI"}
+async def read_tasks(response_model=List[Task]):
+    return tasks
 
+@app.post("/tasks/", response_model=Task)
+async def create_task(task: Task) -> Task:
+    task.id = uuid4()
+    tasks.append(task)
+    return task
 
 if __name__ == "__main__":
     import uvicorn
